07a1cc0e7dd917de981c8a0f593ff8debb53e70d
Your Name <your.email@example.com>
2025-09-23 15:19:32 +0800

    可以读到放大系数了

--- test.py
+++ test.py
@@ -7,3 +7,29 @@ auth = requests.auth.HTTPDigestAuth('admin', 'gene2025')
 response = requests.get(request_url, auth=auth)
 # 输出响应内容
 print(response.text)
+
+response = requests.get("http://192.168.1.64/ISAPI/System/capabilities?type=all", auth=auth)
+# 输出响应内容
+print(response.text)
+
+response = requests.get("http://192.168.1.64/ISAPI/PTZCtrl/channels/1/capabilities", auth=auth)
+# 输出响应内容
+print(response.text)
+
+
+response = requests.get("http://192.168.1.64/ISAPI/PTZCtrl/channels/1/presets", auth=auth)
+# 输出响应内容
+print(response.text)
+
+
+response = requests.get("http://192.168.1.64/ISAPI/PTZCtrl/channels/1/status", auth=auth)
+# 输出响应内容
+print(response.text)
+
+response = requests.get("http://192.168.1.64/ISAPI/Streaming/channels/101/picture", auth=auth)
+if response.status_code == 200:
+    with open('camera_image.jpg', 'wb') as f:
+        f.write(response.content)
+    print("图片保存成功！")
+else:
+    print(f"请求失败，状态码：{response.status_code}")
